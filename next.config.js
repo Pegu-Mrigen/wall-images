950907fcea13b8faff20ab37c98fce98e282fa7c
Pegu-Mrigen <mrig8520@gmail.com>
2023-06-21 18:42:19 +0530

    to deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["demo-ngo.vercel.app", "acb-gohpur.vercel.app"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
